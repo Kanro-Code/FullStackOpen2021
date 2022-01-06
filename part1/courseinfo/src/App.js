e519a183cd8aa02c1a7802c173e6b017ea958256
Kanro <kanro.wow@gmail.com>
2022-01-06 05:38:56 +0100

    exc 1.4 and 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				excercises: 10
+			} ,
+			{
+				name: 'Using props to pass data',
+				excercises: 7
+			},
+			{
+				name: 'State of a component',
+				excercises: 14
+			} 
+		]
+	}
+
+
 
 	return (
 		<div>
-			<Header course={course}/>
-			<Content 
-				part1={part1} exercises1={exercises1} 
-				part2={part2} exercises2={exercises2} 
-				part3={part3} exercises3={exercises3} 
-			/>
-			<Total total={exercises1 + exercises2 + exercises3} />
+			<Header course={course} />
+			<Content parts={parts} />
+			<Total total={parts} />
 		</div>
 	)
 }
