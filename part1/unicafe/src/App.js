02762e9e51a54b25a11ee89b89a1ea9d5103bedd
Kanro <kanro.wow@gmail.com>
2022-01-09 09:57:03 +0100

    1.6 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,10 +5,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  const Feedback = () => {
+    return (
+      <div>
+        <Header text="give feedback" />
+        <Button text="good" handler={() => setGood(good + 1)} />
+        <Button text="neutral" handler={() => setNeutral(neutral + 1)} />
+        <Button text="bad" handler={() => setBad(bad + 1)} />
+      </div>
+    )
+  }
+  
+  const Statistics = () => {
+    return (
+      <div>
+        <Header text="statistics" />
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    )
+  }
+  
+  const Button = ({text, handler}) => (
+    <button onClick={handler}>
+      {text}
+    </button>
+  )
+  
+  const Header = ({text}) => (<h1>{text}</h1>)
+  
   return (
     <div>
-      code here
+      <Feedback />
+      <Statistics />
     </div>
   )
 }
