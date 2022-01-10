a9feb3fc8085e7fb78a05f3a84ae3394586dc2ec
Kanro <kanro.wow@gmail.com>
2022-01-10 07:34:33 +0100

    up to 1.9 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,21 +9,35 @@ const App = () => {
   return (
     <div>
       <h1>feedback</h1>
-        <button onClick={()=>(setGood(good + 1))}>good</button>
-        <button onClick={()=>(setNeutral(neutral + 1))}>neutral</button>
-        <button onClick={()=>(setBad(bad + 1))}>bad</button>
-      <h1>statistics</h1>
+      <button onClick={()=>(setGood(good + 1))}>good</button>
+      <button onClick={()=>(setNeutral(neutral + 1))}>neutral</button>
+      <button onClick={()=>(setBad(bad + 1))}>bad</button>
 
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
 
-const Statistics = () => (
-  good {good}<br />
-  neutral {neutral}<br />
-  bad {bad}<br />
-  average {(good - bad) / (good + neutral + bad)}<br />
-  positive {good/(good + neutral + bad)*100}%<br />
-)
+const Statistics = ({good, neutral, bad}) => {
+  if (good + neutral + bad > 0) {
+    return (
+      <div>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}<br />
+        average {(good - bad) / (good + neutral + bad)}<br />
+        positive {good/(good + neutral + bad)*100}%<br />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+}
+  
 
 export default App
