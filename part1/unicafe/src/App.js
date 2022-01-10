4fcb07823e20b66fec2f8925b5f28c41030c8a3a
Kanro <kanro.wow@gmail.com>
2022-01-10 09:10:41 +0100

    unicafe done, 1.11 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,43 +1,55 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
-  return (
-    <div>
-      <h1>feedback</h1>
-      <button onClick={()=>(setGood(good + 1))}>good</button>
-      <button onClick={()=>(setNeutral(neutral + 1))}>neutral</button>
-      <button onClick={()=>(setBad(bad + 1))}>bad</button>
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+	
+	return (
+		<div>
+			<h1>feedback</h1>
+			<Button text="good" action={() => setGood(good + 1)} />
+			<Button text="neutral" action={() => setNeutral(neutral + 1)} />
+			<Button text="bad" action={() => setBad(bad + 1)} />
 
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-    </div>
-  )
+			<h1>statistics</h1>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	)
 }
 
 const Statistics = ({good, neutral, bad}) => {
-  if (good + neutral + bad > 0) {
-    return (
-      <div>
-        good {good}<br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        average {(good - bad) / (good + neutral + bad)}<br />
-        positive {good/(good + neutral + bad)*100}%<br />
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
+	if (good + neutral + bad > 0) {
+		return (
+			<table>
+				<tbody>
+					<StatisticLine text="good" value={good} />
+					<StatisticLine text="neutral" value={neutral} />
+					<StatisticLine text="bad" value={bad} />
+					<StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+					<StatisticLine text="positive" value={good/(good + neutral + bad)*100 + "%"} />
+				</tbody>
+			</table>
+		)
+	} else {
+		return (
+			<div>
+				No feedback given
+			</div>
+		)
+	}
 }
-  
 
+const StatisticLine = ({text, value}) => (
+	<tr>
+		<td>{text}</td>
+		<td>{value}</td>
+	</tr>
+)
+
+const Button = ({text, action}) => (
+	<button onClick={action}>{text}</button>
+)
+	
 export default App
