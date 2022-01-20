7fc3ac2570c06ad2fe23bed2365ed5fe2afffc5b
Thijs ter Velde <kanro-dev@protonmail.com>
2022-01-20 21:02:30 +0100

    part 3.1 - 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const PORT = 3001
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+const generateId = () => {
+  return Math.floor((Math.random()*100000))
+}
+
+app.get('/', (request, response) => {
+  resonse.send('<h1>Hello world!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  let info = `Phonebook has info for ${persons.length} people <br>` 
+  info += new Date()
+
+  response.send(info)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
+
+  if (!name) {
+    return response.status(400).json({
+      error: 'name is empty'
+    })
+  } else if (!number) {
+    return response.status(400).json({
+      error: 'number is empty'
+    })
+  }
+
+  const uniqueName = persons.filter(p => p.name === name)
+  const uniqueNumber = persons.filter(p => p.number === number)
+  if (uniqueName.length) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  } else if (uniqueNumber.length) {
+    return response.status(400).json({
+      error: 'number must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name, 
+    number
+  }
+
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
