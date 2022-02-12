4e5e712b7af3be8b4ba2a3ae030ebcb39127a3ee
Thijs ter Velde <tervelde.thijs@gmail.com>
2022-02-12 08:38:54 +0100

    5.7

--- part5/frontend/src/components/blogs/Blog.js
+++ part5/frontend/src/components/blogs/Blog.js
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Toggleable from '../Toggleable'
 
 const Blog = ({ blog, handleDelete }) => {
+	const [viewDetails, setViewDetails] = useState(false)
+
+	const toggleView = () => {
+		setViewDetails(!viewDetails)
+	}
+
+	const clickDelete = (id) => {
+		if (window.confirm('Are you sure you want to delete this blog?')) {
+			// handleDelete(id)
+		}
+	}
+
 	const style = {
 		padding: '10px',
 		border: '1px solid black'
 	}
 	return (
 		<div style={style}>
-			{blog.title} - {blog.author}<br />
-			<Toggleable label="show">
-				{blog.url}<br />
-				likes: {blog.likes} <button>like</button><br />
-
-				<button onClick={() => handleDelete(blog.id)}>Delete</button>
-			</Toggleable>
+			{blog.title} - {blog.author}
+			<button onClick={toggleView}>
+				{(viewDetails) ? 'hide' : 'show'}
+			</button>
+			<br/>
+			{(viewDetails) && 
+				<>
+					{blog.url} <br />
+					likes: {blog.likes} <button>like</button>
+					<br />
+					<button onClick={() => clickDelete(blog.id)}>Delete</button>
+				</>
+			}
 		</div>
 	)
 }
